b796d866ba64b03fad64c40f9b97b655c4529f95
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-04-23 14:43:35 +0530

    updated solver template

--- cp/solver.cpp
+++ cp/solver.cpp
@@ -1,83 +1,27 @@
-// C
-#ifndef _GLIBCXX_NO_ASSERT
-#include <cassert>
-#endif
-#include <cctype>
-#include <cfloat>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cwchar>
-#include <cwctype>
-
-#if __cplusplus >= 201103L
-#include <ccomplex>
-#include <cinttypes>
-#include <cstdbool>
-#include <cstdint>
-#endif
-
-// C++
 #include <algorithm>
 #include <bitset>
-#include <complex>
 #include <deque>
-#include <exception>
 #include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
 #include <iostream>
-#include <istream>
-#include <iterator>
 #include <limits>
 #include <list>
-#include <locale>
 #include <map>
-#include <memory>
-#include <new>
-#include <numeric>
 #include <ostream>
 #include <queue>
 #include <set>
-#include <sstream>
 #include <stack>
-#include <stdexcept>
-#include <streambuf>
 #include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
 #include <vector>
-
 #if __cplusplus >= 201103L
 #include <array>
-#include <atomic>
-#include <chrono>
-#include <codecvt>
-#include <condition_variable>
 #include <forward_list>
-#include <future>
 #include <initializer_list>
-#include <mutex>
 #include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
 #include <tuple>
-#include <typeindex>
-#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 #endif
 using namespace std;
-
 
 //Custom types
 typedef long long int lli;
